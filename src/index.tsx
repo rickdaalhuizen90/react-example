dd7fecd4b82d218cad0d8351ac1e81c5f7afb14d
Rick Daalhuizen <rick.daalhuizen@outlook.com>
2025-03-05 16:11:46 +0100

    chore: add react scan

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { scan } from 'react-scan';
 import App from './App';
+
+scan({
+  enabled: true,
+});
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
